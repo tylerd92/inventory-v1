b9d39c822b7baa50f0bf4eff1ccc783ea1f03d73
tylerd92 <twdeans@gmail.com>
2026-01-27 00:44:52 -0500

    updated products and create util method

--- app/models/product.py
+++ app/models/product.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from app.utils.current_datetime import utcnow
 from app.db.base import Base
 
 class Product(Base):
     __tablename__ = "products"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, index=True)
-    quantity = Column(Integer)
-    category = Column(String)
+    name = Column(String(255), index=True)
+    sku = Column(String(100), unique=True, index=True)
+    category = Column(String(100))
+    created_at = Column(DateTime(timezone=True), default=utcnow, nullable=False)
+    updated_at = Column(DateTime(timezone=True), default=utcnow, onupdate=utcnow, nullable=False)
